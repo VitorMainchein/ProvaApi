6942fef3ca018a72131e9c717617230d820164fd
Vitor Mainchein Morales <vi060154@intelbras.com.br>
2024-07-04 09:51:24 -0300

    provaApi

---
+++ src/components/Clubs.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react"
+
+export default function Clubs({ Clube }){
+    const [clubes, setClubes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchClubes = async () => {
+      try {
+        const response = await fetch('https://api.cartola.globo.com/clubes');
+        const data = await response.json();
+        const clubesArray = Object.values(data); 
+        setClubes(clubesArray);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro na busca dos clubes:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchClubes();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <div className="tudo">
+        <ul className="lista">
+          {clubes.map((clube) => (
+            <li key={clube.id}>
+                <div className="time">
+              <img src={clube.escudos['60x60']}/>
+            <div className="clube_apelido">
+                <div className="nome">
+              {clube.nome}
+                </div>
+                <div className="apelido">
+              {clube.apelido}
+                </div>
+            </div>
+              </div>
+            </li>
+          ))}
+        </ul></div>
+      )}
+    </div>
+  );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <>
+        <nav className="cabecalho">
+        <img className="logo" src="senai.png" alt="" />
+        <p>Ian Ferreira - 3B</p>
+        </nav>
+        </>
+    )
+}
